9ff70cee22f116e234fe7a4cef7e016a7af24ab0
Lucas <lucaschang@pgtalk.com>
2024-04-08 18:05:54 +0800

    Make full screen

--- app/src/main/java/app/lucascoffee/youtubelivetv/MainActivity.kt
+++ app/src/main/java/app/lucascoffee/youtubelivetv/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Text
 import app.lucascoffee.youtubelivetv.ui.theme.YoutubeLiveTVTheme
@@ -14,6 +17,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+
         setContent {
             YoutubeLiveTVTheme {
                 YoutubeScreen("Owke6Quk7T0")
