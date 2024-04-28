0581f6ab5ed9feb5d34acff5dd55a9f104511321
Lucas <hisetu@gmail.com>
2024-04-28 23:32:46 +0800

    Fix channels

--- app/src/main/java/app/lucascoffee/youtubelivetv/data/Channels.kt
+++ app/src/main/java/app/lucascoffee/youtubelivetv/data/Channels.kt
@@ -5,15 +5,25 @@ import app.lucascoffee.youtubelivetv.data.model.Channel
 
 val CHANNELS = listOf(
     Channel("台視新聞", "xL0ch83RAK8"),
+    Channel("中視新聞", "TCnaIE_SAtM"),
     Channel("華視新聞", "wM0g8EoUZ_E"),
+    Channel("公視新聞", "quwqlazU-c8"),
+    Channel("公視", "C6gYqSHLRw4"),
+    Channel("公視 台語台", "6KlRR_DGhmI"),
     Channel("鏡新聞", "5n0y6b0Q25o"),
+    Channel("TVBS NEWS", "m_dhMSvUCIc"),
+    Channel("東森LIVE", "LuQ4S-i5zoE"),
+    Channel("SET NEWS", "oZdzzvxTfUY"),
+    Channel("SET iNEWS", "CKjSm5ZeehE"),
+    Channel("民視新聞台", "ylYJSBUgaMA"),
     Channel("寰宇新聞台", "6IquAgfvYmc"),
     Channel("寰宇新聞 台灣台", "w87VGpgd90U"),
-    Channel("中視新聞", "TCnaIE_SAtM"),
-    Channel("中天新聞", "oIgbl7t0S_w"),
+
     Channel("東森財經新聞", "WHEPzbFA3hw"),
-    Channel("TVBS新聞", "4h2T8rOAMdc"),
-    Channel("公視新聞", "quwqlazU-c8"),
+    Channel("57東森財經新聞", "LuQ4S-i5zoE"),
+    Channel("中天新聞", "oIgbl7t0S_w"),
+
+    Channel("台灣地震監視", "Owke6Quk7T0"),
 )
 
 private val list = listOf(
