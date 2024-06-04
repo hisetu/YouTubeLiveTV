c4fdb56c80a5adab091f1fda544921ef21182943
Lucas <hisetu@gmail.com>
2024-06-05 07:22:45 +0800

    Remove unused

--- app/src/main/java/app/lucascoffee/youtubelivetv/YoutubeScreen.kt
+++ app/src/main/java/app/lucascoffee/youtubelivetv/YoutubeScreen.kt
@@ -1,6 +1,5 @@
 package app.lucascoffee.youtubelivetv
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.MutableState
