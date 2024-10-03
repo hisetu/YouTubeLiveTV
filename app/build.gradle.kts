bb224167a1c1dc3fd28594f94baadf8a9264b34d
Lucas <hisetu@gmail.com>
2024-10-03 18:36:58 +0800

    Upgrade Kotlin version to 2.0.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("plugin.serialization") version "2.0.20"
     alias(libs.plugins.compose.compiler)
 }
 
